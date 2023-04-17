257feef009e13ab24766443e39a2014ed91a8713
zolegus <zolegus@gmail.com>
2023-04-17 21:59:55 +0200

    has been added aliases and some config changes in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,13 @@ starship init fish | source
 zoxide init fish | source
 fish_add_path /usr/local/bin 
 fish_add_path ~/.cargo/bin
+
+set fish_greeting ""
+if type -q exa
+	alias ll "exa -l -g --icons"
+	alias lla "ll -a"
+end
+alias g git
+alias t tmux
+command -qv nvim && alias vim nvim
+
