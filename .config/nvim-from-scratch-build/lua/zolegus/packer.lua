0c67963580e966e6575b725a3af9da7ce5eb8a2d
zolegus <zolegus@gmail.com>
2023-04-19 16:55:34 +0200

    added LSP to nvim build

--- .config/nvim-from-scratch-build/lua/zolegus/packer.lua
+++ .config/nvim-from-scratch-build/lua/zolegus/packer.lua
@@ -21,5 +21,33 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+	  },
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},     -- Required
+	  {'hrsh7th/cmp-buffer'},
+	  {'hrsh7th/cmp-path'},
+	  {'saadparwaiz1/cmp_luasnip'},
+	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	  {'hrsh7th/cmp-nvim-lua'},
+
+	  -- Snipets
+	  {'L3MON4D3/LuaSnip'},     -- Required
+	  {'rafamadriz/friendly-snippets'},
+
+  }
+}
 
 end)
