6625216be23b8447bb0019db37ddc6a6055c7fe7
zolegus <zolegus@gmail.com>
2023-06-17 14:05:15 +0200

    refact: little changes

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -2,19 +2,19 @@ require("hop").setup()
 local hop = require("hop")
 local directions = require("hop.hint").HintDirection
 
--- vim.keymap.set("", "t", function()
--- 	hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
--- end, { remap = true })
--- vim.keymap.set("", "T", function()
--- 	hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
--- end, { remap = true })
-
 vim.keymap.set("", "t", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+	hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
 end, { remap = true })
 vim.keymap.set("", "T", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+	hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
 end, { remap = true })
+
+-- vim.keymap.set("", "f", function()
+-- 	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+-- end, { remap = true })
+-- vim.keymap.set("", "F", function()
+-- 	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+-- end, { remap = true })
 
 -- vim.keymap.set("", "t", function()
 -- 	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
