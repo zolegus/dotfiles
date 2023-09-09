7e55b0c51c439eb5a21dca190f467e7a4cf263b1
zolegus <zolegus@gmail.com>
2023-09-09 13:51:50 +0200

    feat: added stylua formater

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,9 +10,9 @@
 -- }
 -- lspconfig.golangci_lint_ls.setup {}
 -- lspconfig.rust_analyzer.setup {
-  -- settings = {
-    -- ['rust-analyzer'] = {
-            -- diagnostics = {
+-- settings = {
+-- ['rust-analyzer'] = {
+-- diagnostics = {
 --                 enable = true,
 --                 experimental = {
 --                     enable = true,
@@ -35,12 +35,12 @@ local icons = require("core.icons")
 -- }
 
 vim.diagnostic.config({
-    -- virtual_text = { prefix = icons.ui.BoldDividerLeft },
-    virtual_text = { prefix = icons.ui.TriangleShortArrowLeft },
-    signs = true, --{ active = signs },
-    underline = true,
-    update_in_insert = true, -- updated diagnostics until you leave insert mode
-    severity_sort = true,
+	-- virtual_text = { prefix = icons.ui.BoldDividerLeft },
+	virtual_text = { prefix = icons.ui.TriangleShortArrowLeft },
+	signs = true, --{ active = signs },
+	underline = true,
+	update_in_insert = true, -- updated diagnostics until you leave insert mode
+	severity_sort = true,
 })
 
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -51,20 +51,21 @@ vim.diagnostic.config({
 --    on_attach = on_attach,
 -- }
 
-local lsp = require('lsp-zero').preset("recommended")
+local lsp = require("lsp-zero").preset("recommended")
 lsp.ensure_installed({
-    'lua_ls',
-    'rust_analyzer',
-    -- 'tsserver',
-    -- 'eslint',
-    -- 'gopls',
-    -- 'bashls',
+	"lua_ls",
+	"rust_analyzer",
+	"stylua",
+	-- 'tsserver',
+	-- 'eslint',
+	-- 'gopls',
+	-- 'bashls',
 })
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 -- require('lspconfig').eslint.setup({
 --   filestypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' },
@@ -75,14 +76,14 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 -- })
 
 -- local on_attach = function(client)
-	-- require("completion").on_attach(client)
-	-- require("lsp.lsp-attach").on_attach()
+-- require("completion").on_attach(client)
+-- require("lsp.lsp-attach").on_attach()
 -- end
 --
 -- require('lspconfig').rust_analyzer.setup({
-    -- capabilities = capabilities,
-    -- on_attach = on_attach,
-    -- settings = {
+-- capabilities = capabilities,
+-- on_attach = on_attach,
+-- settings = {
 --         ['rust-analyzer'] = {
 --             diagnostics = {
 --                 enable = true,
@@ -95,105 +96,105 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 -- })
 
 lsp.skip_server_setup({
-    'rust_analyzer',
+	"rust_analyzer",
 })
 lsp.setup()
 
 require("inlay-hints").setup({
-    only_current_line = false,
-    eol = {
-        right_align = false,
-    }
+	only_current_line = false,
+	eol = {
+		right_align = false,
+	},
 })
 
 -----=====####### RUST ###############################
 local ih = require("inlay-hints")
 vim.api.nvim_set_hl(0, "RustInlayHints", { fg = "#313244" })
 require("rust-tools").setup({
-    server = {
-        settings = {
-            ["rust-analyzer"] = {
-                check = {
-                    command = "clippy",
-                },
-                runnables = {
-                    -- cargoExtraArgs = { "--release" },
-                },
-                cargo = {
-                    autoReload = true,
-                    allFeatures = true,
-                    loadOutDirsFromCheck = true,
-                },
-                lens = {
-                    enable = true,
-                },
-                procMacro = {
-                    enable = true,
-                },
-            },
-        },
-    },
-    tools = {
-        executor = require("rust-tools/executors").termopen,           -- can be quickfix or termopen
-        reload_workspace_from_cargo_toml = true,
-        runnables = {
-            use_telescope = true,
-        },
-        inlay_hints = {
-            auto = true,
-            only_current_line = false,
-            show_parameter_hints = false,
-            parameter_hints_prefix = "<-",
-            other_hints_prefix = "=>",
-            max_len_align = false,
-            max_len_align_padding = 1,
-            right_align = false,
-            right_align_padding = 7,
-            highlight = "InlayHints",
-            -- highlight = "RustInlayHints",
-        },
-        hover_actions = {
-            border = "rounded",
-            -- the border that is used for the hover window
-            -- see vim.api.nvim_open_win()
-            -- border = {
-            --     { "╭", "FloatBorder" },
-            --     { "─", "FloatBorder" },
-            --     { "╮", "FloatBorder" },
-            --     { "│", "FloatBorder" },
-            --     { "╯", "FloatBorder" },
-            --     { "─", "FloatBorder" },
-            --     { "╰", "FloatBorder" },
-            --     { "│", "FloatBorder" },
-            -- },
-            -- Maximal width of the hover window. Nil means no max.
-            max_width = nil,
-            -- Maximal height of the hover window. Nil means no max.
-            max_height = nil,
-            -- whether the hover action window gets automatically focused
-            -- default: false
-            auto_focus = true,
-        },
-        on_initialized = function()
-            ih.set_all()
+	server = {
+		settings = {
+			["rust-analyzer"] = {
+				check = {
+					command = "clippy",
+				},
+				runnables = {
+					-- cargoExtraArgs = { "--release" },
+				},
+				cargo = {
+					autoReload = true,
+					allFeatures = true,
+					loadOutDirsFromCheck = true,
+				},
+				lens = {
+					enable = true,
+				},
+				procMacro = {
+					enable = true,
+				},
+			},
+		},
+	},
+	tools = {
+		executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+		reload_workspace_from_cargo_toml = true,
+		runnables = {
+			use_telescope = true,
+		},
+		inlay_hints = {
+			auto = true,
+			only_current_line = false,
+			show_parameter_hints = false,
+			parameter_hints_prefix = "<-",
+			other_hints_prefix = "=>",
+			max_len_align = false,
+			max_len_align_padding = 1,
+			right_align = false,
+			right_align_padding = 7,
+			highlight = "InlayHints",
+			-- highlight = "RustInlayHints",
+		},
+		hover_actions = {
+			border = "rounded",
+			-- the border that is used for the hover window
+			-- see vim.api.nvim_open_win()
+			-- border = {
+			--     { "╭", "FloatBorder" },
+			--     { "─", "FloatBorder" },
+			--     { "╮", "FloatBorder" },
+			--     { "│", "FloatBorder" },
+			--     { "╯", "FloatBorder" },
+			--     { "─", "FloatBorder" },
+			--     { "╰", "FloatBorder" },
+			--     { "│", "FloatBorder" },
+			-- },
+			-- Maximal width of the hover window. Nil means no max.
+			max_width = nil,
+			-- Maximal height of the hover window. Nil means no max.
+			max_height = nil,
+			-- whether the hover action window gets automatically focused
+			-- default: false
+			auto_focus = true,
+		},
+		on_initialized = function()
+			ih.set_all()
 
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-                pattern = { "*.rs" },
-                callback = function()
-                    local _, _ = pcall(vim.lsp.codelens.refresh)
-                end,
-            })
-        end,
-    },
+			vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+				pattern = { "*.rs" },
+				callback = function()
+					local _, _ = pcall(vim.lsp.codelens.refresh)
+				end,
+			})
+		end,
+	},
 })
 
-require("crates").setup {
-    null_ls = {
-        enabled = true,
-        name = "crates.nvim",
-    },
-    popup = {
-        border = "rounded",
-        autofocus = true,
-    },
-}
+require("crates").setup({
+	null_ls = {
+		enabled = true,
+		name = "crates.nvim",
+	},
+	popup = {
+		border = "rounded",
+		autofocus = true,
+	},
+})
