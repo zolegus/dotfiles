5e8da79b5391ab5ddee4967eb613890f5255fa0a
zolegus <zolegus@gmail.com>
2024-04-20 23:19:01 +0200

    refact: fish rm old lines

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,26 +1,3 @@
-# Fish syntax highlighting
-# set -g fish_color_autosuggestion '555'  'brblack'
-# set -g fish_color_cancel -r
-# set -g fish_color_command --bold
-# set -g fish_color_comment red
-# set -g fish_color_cwd green
-# set -g fish_color_cwd_root red
-# set -g fish_color_end brmagenta
-# set -g fish_color_error brred
-# set -g fish_color_escape 'bryellow'  '--bold'
-# set -g fish_color_history_current --bold
-# set -g fish_color_host normal
-# set -g fish_color_match --background=brblue
-# set -g fish_color_normal normal
-# set -g fish_color_operator bryellow
-# set -g fish_color_param cyan
-# set -g fish_color_quote yellow
-# set -g fish_color_redirection brblue
-# set -g fish_color_search_match 'bryellow'  '--background=brblack'
-# set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-# set -g fish_color_user brgreen
-# set -g fish_color_valid_path --underline
-#
 starship init fish | source
 zoxide init fish | source
 
@@ -59,7 +36,7 @@ set -Ux FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}' --border \
 set -Ux FZF_TMUX_OPTS "-p 80%,70%"
 set -gx GPG_TTY (tty)
 
-set -x http_proxy http://127.0.0.1:1087; set -x https_proxy http://127.0.0.1:1087
+# set -x http_proxy http://127.0.0.1:1087; set -x https_proxy http://127.0.0.1:1087
 load_ssh_keys
 
 # if type -q exa
