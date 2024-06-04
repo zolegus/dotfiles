33e2451a4623bebfe7f022a78e2a959b84cefeec
zolegus <zolegus@gmail.com>
2024-06-04 12:08:55 +0200

    fix: nvim lldb adapter

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -10,9 +10,16 @@ local neodev = require("neodev")
 
 dap.adapters.lldb = {
 	type = "executable",
-	command = "lldb-vscode",
+	command = "lldb-dap", -- path
 	name = "lldb",
 }
+
+dap.adapters.gdb = {
+	type = "executable",
+	command = "gdb",
+	args = { "-i", "dap" },
+}
+
 local lldb = {
 	name = "Launch lldb",
 	type = "lldb", -- matches the adapter
@@ -26,8 +33,41 @@ local lldb = {
 	runInTerminal = false,
 }
 
+local gdb = {
+	name = "Launch",
+	type = "gdb",
+	request = "launch",
+	program = function()
+		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+	end,
+	cwd = "${workspaceFolder}",
+	stopAtBeginningOfMainSubprogram = false,
+}
+
 dap.configurations.rust = {
+	-- gdb,
 	lldb,
+	{
+		initCommands = function()
+			-- Find out where to look for the pretty printer Python module
+			local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+			-- llm-tools component needed and python support
+			local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+			local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+			local commands = {}
+			local file = io.open(commands_file, "r")
+			if file then
+				for line in file:lines() do
+					table.insert(commands, line)
+				end
+				file:close()
+			end
+			table.insert(commands, 1, script_import)
+
+			return commands
+		end,
+	},
 }
 --TODO: Refactoring
 vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint" })
