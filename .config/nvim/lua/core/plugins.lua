98a76b4d59d31fe4a84d61f30db750529db04fbe
zolegus <zolegus@gmail.com>
2024-06-09 22:06:21 +0200

    feat: nvim rest plugin

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -538,4 +538,22 @@ require("lazy").setup({
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+
+	-----=====##### REST
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
+	},
+	{
+		"rest-nvim/rest.nvim",
+		ft = "http",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("rest-nvim").setup()
+		end,
+	},
 })
