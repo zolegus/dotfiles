025eedb33618067474cff48c885d53a035898f1d
zolegus <zolegus@gmail.com>
2024-08-03 13:09:26 +0200

    refact: nvim updated 0.10.1, some changes

--- .config/nvim/lua/plugins/pairs.lua
+++ .config/nvim/lua/plugins/pairs.lua
@@ -47,9 +47,8 @@ require("mini.pairs").setup({
 		["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
 		["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 		[">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
-
 		['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-		["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+		["'"] = { action = "close", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
 		["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
 	},
 })
